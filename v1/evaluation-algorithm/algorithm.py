eafc5c1ad9753ce75f6e3f40d318c50ad7a86a57
MoloZzz <mega_oleksii@ukr.net>
2025-05-14 22:35:44 +0300

    feat: update v1

--- v1/evaluation-algorithm/algorithm.py
+++ v1/evaluation-algorithm/algorithm.py
@@ -1,30 +1,32 @@
 import json
 
-with open('./knowledge-base/knowledge_base3.json', 'r', encoding='utf-8') as f:
+with open('./knowledge-base/disease_symptoms.json', 'r', encoding='utf-8') as f:
     disease_db = json.load(f)
 
 # Значення: 0.0 — немає, 0.5 — помірно, 1.0 — сильно
 user_symptoms = {
-    "high_fever": 0.7,
-    "runny_nose": 0.5,
-    "sore_throat": 0.5,
-    'loss_of_smell': 0.7,
+    "fever": 0.5,
+    "headache": 0.5,
+    "muscle pain": 0.5,
+    'sore throat': 0.5,
 }
 
 def score_diseases(user_symptoms, disease_db):
     scores = {}
 
     for disease, symptoms in disease_db.items():
-        score = 0
-        max_possible = 0
-        for symptom, intensity in user_symptoms.items():
-            weight = symptoms.get(symptom)
-            if weight:
-                score += weight * intensity
-                max_possible += weight 
+        total_score = 0.0
+        count = 0
 
-        if max_possible > 0:
-            scores[disease] = round((score / max_possible) * 100, 2)
+        for symptom, user_value in user_symptoms.items():
+            disease_value = symptoms.get(symptom)
+            if disease_value is not None:
+                similarity = 1 - abs(user_value - disease_value)  # the closer, the better
+                total_score += similarity
+                count += 1
+
+        if count > 0:
+            scores[disease] = round((total_score / count) * 100, 2)
         else:
             scores[disease] = 0.0
 
